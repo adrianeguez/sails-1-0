ca33d4a3424a94e7bb0b1874d078d7c6e7aa6b53
Adrian Eguez <eadepto@hotmail.com>
2018-06-07 23:29:12 -0500

    Crear app

---
+++ App/api/models/Libro.js
@@ -0,0 +1,24 @@
+/**
+ * Libro.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    icbn: {
+      type: 'string',
+      required: true
+    },
+    nombre: {
+      type: 'string'
+    },
+    userId: {
+      model: 'user'
+    }
+  },
+
+};
+
---
+++ App/api/models/User.js
@@ -0,0 +1,20 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: {type: 'string', required: true},
+    apellido: {type: 'string', required: true},
+    libros: {
+      collection: 'libro',
+      via: 'userId'
+    }
+  },
+
+};
+
